1e73603b9b78c6bf07908b4323fb104e675892c2
rosenzhelyazkov <0zhelr65@solent.ac.uk>
2025-11-22 15:26:31 +0000

    make some songs

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,19 @@
 fun main() {
+    //create some sample songs
+    val s1=Song("Track One", "Artist A",3.5)
+    val s2=Song("Track Two", "Artist B",4.1)
+    val s3=Song("Track Three", "Artist A",2.9)
+
+    //create an album containing the songs
+    val album=Album(listof(s1,s2,s3))
+
+    //cal the new method to get ony songs by "Artist A"
+    val artistASongs=album.filterByArtist("Artist A")
+
+    //Print the titles of the songs returned (could be empty)
+    println("Songs by Artist A")
+    for(song in artistASongs){
+        println("-${song.title}")
+    }
 
 }
